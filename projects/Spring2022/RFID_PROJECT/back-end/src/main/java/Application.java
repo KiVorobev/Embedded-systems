d7c9c6eaff17c2b577d81db21bf68d873cad72e5
Matvey Chukhno <mr.chucno@yandex.ru>
2022-06-07 07:40:34 +0300

    Order endpoints in blocks

--- projects/Spring2022/RFID_PROJECT/back-end/src/main/java/Application.java
+++ projects/Spring2022/RFID_PROJECT/back-end/src/main/java/Application.java
@@ -5,24 +5,23 @@ import controller.UserController;
 import io.javalin.Javalin;
 import util.PropertiesUtil;
 
-import static io.javalin.plugin.rendering.template.TemplateUtil.model;
-
 public class Application {
 
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(Integer.parseInt(PropertiesUtil.get("port")));
 
+        app.get("/start", EnterHistoryController::getAllLastActivities);
+        app.get("/scanners", ScannerController::renderScannerPage);
+
         app.get("/user/get/{id}", UserController::getUser);
         app.get("/user/edit/{id}", UserController::editUser);
-        app.before("/user/get/{id}", ctx -> {
-            System.out.println("before");
-        });
         app.post("/user/add", UserController::addUser);
         app.delete("/user/delete/{id}", UserController::deleteUser);
         app.post("/user/update", UserController::updateUser);
+        app.delete("/user/delete/activities/{id}", EnterHistoryController::deleteActivityList);
 
-        app.get("/scanner/put" , ScannerController::renderAddScannerPage);
-        app.get("/scanner/remove" , ScannerController::renderRemovePage);
+        app.get("/scanner/put", ScannerController::renderAddScannerPage);
+        app.get("/scanner/remove", ScannerController::renderRemovePage);
         app.post("/scanner/add", ScannerController::addScanner);
         app.get("/scanner/get/{hardwareNumber}", ScannerController::findScannerByHardwareNumber);
         app.delete("/scanner/delete/{hardwareNumber}", ScannerController::removeScannerByHardwareNumber);
@@ -31,17 +30,8 @@ public class Application {
         app.post("/mcu/add", MCUController::addMCU);
         app.get("/mcu/get/{address}", MCUController::findByAddress);
 
-//        Imitation of websocket MCU request to enter the user:
         app.post("/mcu/enter", ScannerController::verifyEnter);
 
-        app.ws("/websocket", ws -> {
-            ws.onConnect(ctx -> System.out.println("Connected"));
-        });
-
-        app.get("/start", EnterHistoryController::getAllLastActivities);
-        app.delete("/user/delete/activities/{id}", EnterHistoryController::deleteActivityList);
-
-        app.get("/scanners", ScannerController::renderScannerPage);
         app.get("/*", context -> {
             context.redirect("/start");
         });
