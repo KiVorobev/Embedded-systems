8dfce3e547d20ff10195492f1685b28911411b7b
Matvey Chukhno <mr.chucno@yandex.ru>
2022-06-14 17:53:29 +0300

    Add udp server thread

--- projects/Spring2022/RFID_PROJECT/back-end/src/main/java/Application.java
+++ projects/Spring2022/RFID_PROJECT/back-end/src/main/java/Application.java
@@ -3,18 +3,28 @@ import controller.MCUController;
 import controller.ScannerController;
 import controller.UserController;
 import io.javalin.Javalin;
+import udpserver.Server;
 import util.PropertiesUtil;
+
+////TODO
+//-поиск пользователя по номеру карты
+//-доп поток, который на юдп просто слушает датаграм сокет
 
 public class Application {
 
     public static void main(String[] args) {
-        Javalin app = Javalin.create( config ->{
+        Javalin app = Javalin.create(config -> {
             config.contextPath = "/";
             config.enableDevLogging();
         }).start(Integer.parseInt(PropertiesUtil.get("port")));
+        Thread serverThread = new Thread(new Server());
+        serverThread.start();
 
         app.get("/start", EnterHistoryController::getAllLastActivities);
         app.get("/scanners", ScannerController::renderScannerPage);
+        app.get("/user/put", UserController::renderAddUserPage);
+        app.get("/user/search", UserController::renderSearchUserPage);
+
 
         app.get("/user/get/{id}", UserController::getUser);
         app.get("/user/edit/{id}", UserController::editUser);
@@ -22,6 +32,7 @@ public class Application {
         app.delete("/user/delete/{id}", UserController::deleteUser);
         app.post("/user/update", UserController::updateUser);
         app.delete("/user/delete/activities/{id}", EnterHistoryController::deleteActivityList);
+        app.post("/user/search", UserController::getUserByCardId);
 
         app.get("/scanner/put", ScannerController::renderAddScannerPage);
         app.get("/scanner/remove", ScannerController::renderRemovePage);
---
+++ projects/Spring2022/RFID_PROJECT/back-end/src/main/java/udpserver/Server.java
@@ -0,0 +1,53 @@
+package udpserver;
+
+import exception.DoesntExistException;
+import io.javalin.Javalin;
+import service.ScannerService;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.*;
+import java.nio.file.Path;
+
+public class Server implements Runnable {
+
+    private byte[] receivingDataBuffer = new byte[256];
+    private byte[] sendingDataBuffer = new byte[256];
+    private ScannerService scannerService;
+
+    public Server() {
+        scannerService = new ScannerService();
+    }
+
+    @Override
+    public void run() {
+        File file = Path.of("src", "main", "java", "udpserver", "server_log.txt").toFile();
+        try (DatagramSocket datagramSocket = new DatagramSocket(1234);
+             FileOutputStream outputStream = new FileOutputStream(file);) {
+            while (true) {
+                DatagramPacket inputPacket = new DatagramPacket(receivingDataBuffer, receivingDataBuffer.length);
+                datagramSocket.receive(inputPacket);
+                InetAddress inetAddress = inputPacket.getAddress();
+                int port = inputPacket.getPort();
+                String requestFromCard = new String(inputPacket.getData(), 0, inputPacket.getLength());
+                scannerService.verifyEnter(requestFromCard, 1l);
+                outputStream.write(requestFromCard.getBytes());
+                outputStream.write(System.lineSeparator().getBytes());
+                String messageToCard = "Message";
+                sendingDataBuffer = messageToCard.getBytes();
+                DatagramPacket outputPacket = new DatagramPacket(sendingDataBuffer, sendingDataBuffer.length, inetAddress, port);
+                datagramSocket.send(outputPacket);
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (DoesntExistException e) {
+            System.out.println("User doesn't exist");
+            e.printStackTrace();
+        }
+    }
+}
